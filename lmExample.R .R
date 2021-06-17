aa29eb50a382bdf8047cd663097abd18327f157e
Ks973 <keananbeatty@msn.com>
2021-06-17 02:20:45 -0600

    Additions

--- lmExample.R .R
+++ lmExample.R .R
@@ -1,8 +1,14 @@
 # Module 13 R data to Github. 
 
+#lm data
 x = rnorm(100, mean = 20, sd = 5)
 y = x + rnorm(100, mean = 0, sd = 2)
 
 fit = lm(y ~ x)
 
 summary(fit)
+
+#Plot
+plot(x,y)
+abline(fit, col=2)
+
